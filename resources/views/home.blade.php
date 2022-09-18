502e5317f7c61725e6a56d64a37336b31790ee71
Jochem de Wit <s1133680@student.hsleiden.nl>
2022-09-19 00:12:18 +0200

    First view using routing and blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('page-stylesheet')
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endsection
+@section('header')
+    <header class="home-header">
+        <p class="home-header__title">Portfolio website</p>
+    </header>
+@endsection
+@section('main')
+    <main></main>
+@endsection
