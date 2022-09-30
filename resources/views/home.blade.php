1808e5f2c95e2690d92f10a77c9a9255185f2543
Jochem de Wit <s1133680@student.hsleiden.nl>
2022-09-30 16:25:35 +0200

    made whole website responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,24 @@
 @extends('layout')
 
 @section('header')
+    <div class="home-header__ham">
+        <div class="hamburger-menu" id="nav">
+            <svg class="ham hamRotate ham" id="navIcon" viewBox="0 0 100 100" width="50" onclick="this.classList.toggle('active')">
+                <path
+                    class="line top"
+                    d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
+                <path
+                    class="line middle"
+                    d="m 30,50 h 40" />
+                <path
+                    class="line bottom"
+                    d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
+            </svg>
+        </div>
+    </div>
     <header class="home-header">
         <h1 class="home-header__title">Portfolio website</h1>
-        <div class="home-header__ham">
-            <div class="hamburger-menu" id="nav">
-                <svg class="ham hamRotate ham" id="navIcon" viewBox="0 0 100 100" width="50" onclick="this.classList.toggle('active')">
-                    <path
-                        class="line top"
-                        d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
-                    <path
-                        class="line middle"
-                        d="m 30,50 h 40" />
-                    <path
-                        class="line bottom"
-                        d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
-                </svg>
-            </div>
-        </div>
-        <div class="home-header__rectangle">
-        </div>
+        <div class="home-header__rectangle"></div>
         <figure class="home-header__image-container">
             <div class="image-container__left-text">
                 <p class="left-text__p">
--- resources/views/over-mij.blade.php
+++ resources/views/over-mij.blade.php
@@ -17,7 +17,6 @@
                 keuken en de mensen om me heen zijn over het algemeen veel minder gestresst, maar de discipline en het structureel nadenken heb ik meegenomen.
             </p>
         </article>
-
         <article class="main__am-properties">
             <h2 class="am-properties__title am-title">Soft skills: eigenschappen</h2>
             <figure class="am-properties__pr-figure">
@@ -44,121 +43,127 @@
                 Positiviteit uitstralen doe ik onbewust, maar dit wordt vaak gewaardeerd door collega’s.
             </p>
         </article>
-
+        <div class="horizontal-box">
         <article class="main__am-knowledge">
-            <h2 class="am-knowledge__title am-title">Hard skills: kennis</h2>
-            <p class="am-knowledge--text am-text">
-                Inmiddels ben ik 4 jaar aan het programmeren en heb ik kennis opgedaan van verschillende
-                programmeertalen en frameworks tijdens de opleiding mbo media & applicatie ontwikkelaar en hbo
-                informatica. Ook heb ik stage gelopen bij een front-end bedrijf en een back-end bedrijf en heb ik een
-                half jaar een bijbaan gehad naast mijn hbo als creative front-end developer. <br>
-                <a class="am-text__link" href="{{ url('/mijn-werk') }}">Lees meer over mijn werkervaringen…</a>
-                <br><br>
-                Hieronder staan de programmeertalen waarmee ik heb gewerkt.
-            </p>
-            <h3 class="am-subtitle">Front-end</h3>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">HTML</h4>
-                <p class="language__text am-text">
-                    De eerste taal waarmee ik code begon te schrijven, en waar ik vervolgens veel advertentie banners
-                    en web pagina’s mee heb gemaakt in combinatie met javascript, css en php.
+                <h2 class="am-knowledge__title am-title">Hard skills: kennis</h2>
+                <p class="am-knowledge--text am-text">
+                    Inmiddels ben ik 4 jaar aan het programmeren en heb ik kennis opgedaan van verschillende
+                    programmeertalen en frameworks tijdens de opleiding mbo media & applicatie ontwikkelaar en hbo
+                    informatica. Ook heb ik stage gelopen bij een front-end bedrijf en een back-end bedrijf en heb ik een
+                    half jaar een bijbaan gehad naast mijn hbo als creative front-end developer. <br>
+                    <a class="am-text__link" href="{{ url('/mijn-werk') }}">Lees meer over mijn werkervaringen…</a>
+                    <br><br>
+                    Hieronder staan de programmeertalen waarmee ik heb gewerkt.
                 </p>
-            </section>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">CSS</h4>
-                <p class="language__text am-text">
-                    Met CSS heb ik veel advertentie banners voorzien van styling en heb ik webpagina’s er beter uit
-                    laten zien. Ook heb ik gewerkt met toevoegingen zoals bootstrap, SCSS en Sass, en hanteer ik graag
-                    de BEM-methode.
-                </p>
-            </section>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">JS</h4>
-                <p class="language__text am-text">
-                    Met javascript heb ik veel advertenties interactief gemaakt door animaties toe te voegen, pop-up
-                    vensters te laten openen en carousels te maken.<br>
-                    Ook heb ik gewerkt met frameworks zoals React, Veu en jQuery.
-                </p>
-            </section>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">Dart</h4>
-                <p class="language__text am-text">
-                    Door middel van Flutter heb ik met dart een app gebouwd die zowel voor apple als android compiled
-                    en ook in de browser voor desktops te gebruiken is.
-                </p>
-            </section>
-            <h3 class="am-subtitle">Back-end</h3>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">PHP</h4>
-                <p class="language__text am-text">
-                    In PHP heb ik object-georiënteerd leren programmeren, en vervolgens de mvc-structuur aangeleerd.
-                    <br>Nu werk ik graag met PHP in het framework Laravel.
-                </p>
-            </section>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">Java</h4>
-                <p class="language__text am-text">
-                    Met java begon ik met kleine command-line applicaties, en vervolgens leerde ik over JavaFX. Hierin
-                    maakte ik kleine interfaces met buttons en interacties, en vervolgens maakte ik een game met
-                    JavaFXGL.
-                </p>
-            </section>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">Python</h4>
-                <p class="language__text am-text">
-                    Pas na drie jaar programmeren startte ik met Python, waarmee ik bijvoorbeeld leerde om teksten uit
-                    webpagina’s te scrapen en een wordcloud te genereren met de meest belangrijke woorden uit de tekst.
-                </p>
-            </section>
-            <section class="am-knowledge__language">
-                <h4 class="language__subject">SQL</h4>
-                <p class="language__text am-text">
-                    Met SQL kan ik commands uitvoeren in multi-relationele databases om nieuwe tabellen aan te maken,
-                    data toe te voegen, relaties tussen data te creëren en efficiënt data op te halen.
-                </p>
-            </section>
-        </article>
+                <h3 class="am-subtitle">Front-end</h3>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">HTML</h4>
+                    <p class="language__text am-text">
+                        De eerste taal waarmee ik code begon te schrijven, en waar ik vervolgens veel advertentie banners
+                        en web pagina’s mee heb gemaakt in combinatie met javascript, css en php.
+                    </p>
+                </section>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">CSS</h4>
+                    <p class="language__text am-text">
+                        Met CSS heb ik veel advertentie banners voorzien van styling en heb ik webpagina’s er beter uit
+                        laten zien. Ook heb ik gewerkt met toevoegingen zoals bootstrap, SCSS en Sass, en hanteer ik graag
+                        de BEM-methode.
+                    </p>
+                </section>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">JS</h4>
+                    <p class="language__text am-text">
+                        Met javascript heb ik veel advertenties interactief gemaakt door animaties toe te voegen, pop-up
+                        vensters te laten openen en carousels te maken.<br>
+                        Ook heb ik gewerkt met frameworks zoals React, Veu en jQuery.
+                    </p>
+                </section>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">Dart</h4>
+                    <p class="language__text am-text">
+                        Door middel van Flutter heb ik met dart een app gebouwd die zowel voor apple als android compiled
+                        en ook in de browser voor desktops te gebruiken is.
+                    </p>
+                </section>
+                <h3 class="am-subtitle">Back-end</h3>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">PHP</h4>
+                    <p class="language__text am-text">
+                        In PHP heb ik object-georiënteerd leren programmeren, en vervolgens de mvc-structuur aangeleerd.
+                        <br>Nu werk ik graag met PHP in het framework Laravel.
+                    </p>
+                </section>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">Java</h4>
+                    <p class="language__text am-text">
+                        Met java begon ik met kleine command-line applicaties, en vervolgens leerde ik over JavaFX. Hierin
+                        maakte ik kleine interfaces met buttons en interacties, en vervolgens maakte ik een game met
+                        JavaFXGL.
+                    </p>
+                </section>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">Python</h4>
+                    <p class="language__text am-text">
+                        Pas na drie jaar programmeren startte ik met Python, waarmee ik bijvoorbeeld leerde om teksten uit
+                        webpagina’s te scrapen en een wordcloud te genereren met de meest belangrijke woorden uit de tekst.
+                    </p>
+                </section>
+                <section class="am-knowledge__language">
+                    <h4 class="language__subject">SQL</h4>
+                    <p class="language__text am-text">
+                        Met SQL kan ik commands uitvoeren in multi-relationele databases om nieuwe tabellen aan te maken,
+                        data toe te voegen, relaties tussen data te creëren en efficiënt data op te halen.
+                    </p>
+                </section>
+            </article>
 
-        <article class="main__am-expertise">
-            <h2 class="am-expertise__title am-title">Expertise</h2>
-            <h3 class="am-subtitle am-expertise__subtitle">Back-end</h3>
-            <p class="am-text">
-                Nu ik ervaring heb opgedaan in front-end development, wat ik erg leuk maar toch beperkt vond, wil ik me
-                graag meer verdiepen in back-end development. Het creëren van logische oplossingen voor software met
-                inlog-systemen, database connecties en koppelingen naar andere systemen spreekt mij erg aan.
-                Ondanks dat dit vaak samen gaat met grote verantwoordelijkheid en ondanks dat ik er nog een hoop voor
-                moet leren, lijkt dit mij een uitdaging waar ik me 100% voor wil inzetten.
-            </p>
-            <h3 class="am-subtitle am-expertise__subtitle">Cybersecurity & Forensisch ICT</h3>
-            <p class="am-text">
-                Op het mbo en hbo heb ik ook les gekregen in cybersecurity en forensisch ICT. Encrypties, speuren en
-                filteren door images van harde schijven op zoek naar bewijsmateriaal en dus ook leren hoe je het beste
-                materiaal kan verbergen geeft mij een kick, alleen vond ik het documenteren minder interessant en helaas
-                ging daar de meeste tijd in zitten.
-            </p>
-            <h3 class="am-subtitle am-expertise__subtitle">Big data management</h3>
-            <p class="am-text">
-                Ook kreeg ik les in big data management, het verwerken van grote hoeveelheid data en daar nuttige
-                informatie uithalen met behulp van Python script, SQL queries en PowerBI rapporten. Ik vond het erg
-                leerzaam en fijn om hier wat vanaf te weten, maar het werk gaf me weinig voldoening.
-            </p>
-            <h3 class="am-subtitle am-expertise__subtitle">UI/UX</h3>
-            <p class="am-text">
-                Waar ik vooral in het begin van mijn leerproces mijn interesse lag was het optimaliseren van user
-                interfaces en user experience. Ik wist toen nog niet zo veel van graphic design, maar inmiddels heb ik
-                hier veel over geleerd bij GumGum, waar ik responsive advertentie banners programmeerde.
-            </p>
-        </article>
+
+            <article class="main__am-expertise">
+                <h2 class="am-expertise__title am-title">Expertise</h2>
+                <h3 class="am-subtitle am-expertise__subtitle">Back-end</h3>
+                <p class="am-text">
+                    Nu ik ervaring heb opgedaan in front-end development, wat ik erg leuk maar toch beperkt vond, wil ik me
+                    graag meer verdiepen in back-end development. Het creëren van logische oplossingen voor software met
+                    inlog-systemen, database connecties en koppelingen naar andere systemen spreekt mij erg aan.
+                    Ondanks dat dit vaak samen gaat met grote verantwoordelijkheid en ondanks dat ik er nog een hoop voor
+                    moet leren, lijkt dit mij een uitdaging waar ik me 100% voor wil inzetten.
+                </p>
+                <h3 class="am-subtitle am-expertise__subtitle">Cybersecurity & Forensisch ICT</h3>
+                <p class="am-text">
+                    Op het mbo en hbo heb ik ook les gekregen in cybersecurity en forensisch ICT. Encrypties, speuren en
+                    filteren door images van harde schijven op zoek naar bewijsmateriaal en dus ook leren hoe je het beste
+                    materiaal kan verbergen geeft mij een kick, alleen vond ik het documenteren minder interessant en helaas
+                    ging daar de meeste tijd in zitten.
+                </p>
+                <h3 class="am-subtitle am-expertise__subtitle">Big data management</h3>
+                <p class="am-text">
+                    Ook kreeg ik les in big data management, het verwerken van grote hoeveelheid data en daar nuttige
+                    informatie uithalen met behulp van Python script, SQL queries en PowerBI rapporten. Ik vond het erg
+                    leerzaam en fijn om hier wat vanaf te weten, maar het werk gaf me weinig voldoening.
+                </p>
+                <h3 class="am-subtitle am-expertise__subtitle">UI/UX</h3>
+                <p class="am-text">
+                    Waar ik vooral in het begin van mijn leerproces mijn interesse lag was het optimaliseren van user
+                    interfaces en user experience. Ik wist toen nog niet zo veel van graphic design, maar inmiddels heb ik
+                    hier veel over geleerd bij GumGum, waar ik responsive advertentie banners programmeerde.
+                </p>
+            </article>
+        </div>
+
+
         <article class="main__am-short">
             <h2 class="am-title">In het kort</h2>
             <p class="am-text">
-                Naast alle professionele vaardigheden vertel ik graag nog even in het kort over wie ik nou eigenlijk
-                ben. <br>Ik ben Jochem de Wit, 24 jaar oud en gepassioneerd skateboarder. Ik ben opgegroeid in
-                Valkenburg (ZH), maar inmiddels woon ik in Den Haag, waar meer mogelijkheden zijn voor werk en skaten.
-                Ik heb regelmatig behoefte om mijn hoofd leeg te maken en te ontspannen, dan duik ik graag de natuur in
-                om te wandelen, of ga ik naar de zee en het strand.
+                Naast alle professionele vaardigheden vertel ik nog even in het kort wie ik ben en wat ik doe in mijn
+                vrije tijd.<br>Ik ben Jochem de Wit, 24 jaar oud en gepassioneerd skateboarder. Ik ben opgegroeid in
+                Valkenburg (ZH) maar inmiddels woon ik in Den Haag, waar meer mogelijkheden zijn voor werk en skaten.
+                Wanneer ik behoefte heb om mijn hoofd leeg te maken en te ontspannen dan duik ik graag de natuur in
+                om te wandelen, of dan ga ik naar de zee en het strand.
             </p>
         </article>
+
+
     </main>
 @endsection
 
